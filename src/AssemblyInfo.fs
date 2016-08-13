bbd97277b3b81c1506eafc056fc36b86431148a5
Rahul Kapoor <rk@trie.org>
2016-08-13 00:55:26 -0400

    Remove versioning that depended on Hg

---
+++ src/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+#nowarn "2003"
+
+namespace BucklingSprings.Aware
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("1.0.500.0")>]
+[<assembly: AssemblyFileVersion("1.0.500.0")>]
+[<assembly: AssemblyDescription("Aware")>]
+[<assembly: AssemblyInformationalVersionAttribute ("1.0.500.0")>]
+[<assembly: AssemblyProduct("Aware 1.0.500.0")>]
+[<assembly: AssemblyCopyright("Buckling springs LLC")>]
+[<assembly: ComVisible(false)>]
+
+()
---
+++ src/AssemblyInfo.vb
@@ -0,0 +1,8 @@
+Imports System.Reflection
+
+<Assembly: AssemblyVersion("1.0.500.0")> 
+<Assembly: AssemblyFileVersion("1.0.500.0")> 
+<Assembly: AssemblyDescription("Aware")> 
+<Assembly: AssemblyInformationalVersionAttribute("1.0.500.0")> 
+<Assembly: AssemblyProduct("Aware 1.0.500.0")> 
+<Assembly: AssemblyCopyright("Buckling springs LLC")> 
